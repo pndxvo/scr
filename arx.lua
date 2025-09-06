ac858b74558b2d4c2a08210b36bd39a80db6d08e
pndxvo <panitan.poundx@gmail.com>
2025-09-06 23:46:53 +0700

    arx.lua

--- arx.lua
+++ arx.lua
@@ -3,18 +3,25 @@ local DiscordLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/pn
 getgenv().settings = {
     gamespeed = false,
     retry = false,
-	retryf = false,
+    retrywavetwo = false,
     next = false,
+    retryevent = false,
     notify = false
 }
 
 local win = DiscordLib:Window("pndx")
-local serv = win:Server("Main", "http://www.roblox.com/asset/?id=6031075938")
+local serv = win:Server("Main", "")
 local ntfy = serv:Channel("Auto Farm")
 
 local Players = game:GetService("Players") 
 local plr = Players.LocalPlayer
 local playerGui = plr:WaitForChild("PlayerGui")
+
+local bb = game:GetService("VirtualUser")
+plr.Idled:connect(function()
+    bb:CaptureController()
+    bb:ClickButton2(Vector2.new())
+end)
 
 ntfy:Toggle("Game Speed 3x", false, function(b)
     settings.gamespeed = b
@@ -35,20 +42,32 @@ ntfy:Toggle("Auto Retry", false, function(b)
                 task.wait(1)
                 game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("Server"):WaitForChild("OnGame"):WaitForChild("Voting"):WaitForChild("VoteRetry"):FireServer()
             end
-            task.wait(0.1)
+            task.wait(0.5)
         end
     end)
 end)
 
 ntfy:Toggle("Auto Retry [FAST]", false, function(b)
-    settings.retryf = b
+    settings.retryevent = b
     task.spawn(function()
-        while settings.retryf do
-            -- if playerGui:FindFirstChild("GameEndedAnimationUI") then
-                -- task.wait(1)
+        while settings.retryevent do
                 game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("Server"):WaitForChild("OnGame"):WaitForChild("Voting"):WaitForChild("VoteRetry"):FireServer()
-            -- end
             task.wait(0.1)
+        end
+    end)
+end)
+
+ntfy:Toggle("Bug Event", false, function(b)
+    settings.retrywavetwo = b
+    local remote = game:GetService("ReplicatedStorage").Remote.Server.OnGame.RestartMatch
+    local cw = game:GetService("ReplicatedStorage"):WaitForChild("Values"):WaitForChild("Waves"):WaitForChild("CurrentWave")
+    task.spawn(function()
+        while settings.retrywavetwo do
+            if cw.Value == 2 then
+                remote:FireServer(r)
+                task.wait(2)
+            end
+            task.wait(0.5)
         end
     end)
 end)
@@ -57,11 +76,11 @@ ntfy:Toggle("Auto Next", false, function(b)
     settings.next = b
     task.spawn(function()
         while settings.next do
-            -- if playerGui:FindFirstChild("GameEndedAnimationUI") then
-                -- task.wait(0.1)
+            if playerGui:FindFirstChild("GameEndedAnimationUI") then
+                task.wait(2)
                 game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("Server"):WaitForChild("OnGame"):WaitForChild("Voting"):WaitForChild("VoteNext"):FireServer()
-            -- end
-            task.wait(0.1)
+            end
+            task.wait(0.5)
         end
     end)
 end)
@@ -77,6 +96,7 @@ local function sendWebhook(ui, x)
             {
                 title = ("%s"):format(plr.Name),
                 color = 65535,
+                thumbnail = {url = "https://www.roblox.com/headshot-thumbnail/image?userId=".. game.Players.LocalPlayer.UserId .."&width=420&height=420&format=png"},
                 fields = {
                     { 
                         name = "Result", 
@@ -84,7 +104,7 @@ local function sendWebhook(ui, x)
                     },
                     { 
                         name = "Time Total", 
-                        value = ("%s วินาที"):format(x.Value)
+                        value = ("%s"):format(x.Value)
                     }
                 },
                 timestamp = DateTime.now():ToIsoDate(),
@@ -125,12 +145,81 @@ ntfy:Toggle("Webhook", false, function(b)
     end
 end)
 
-ntfy:Button("Anti AFK", function ()
-	local bb = game:GetService("VirtualUser")
-	plr.Idled:connect(function()
-		bb:CaptureController()
-		bb:ClickButton2(Vector2.new())
-	end)
-	DiscordLib:Notification("Anti AFK", "Enable ✅", "Accept")
-end)
+local serv = win:Server("Misc", "http://www.roblox.com/asset/?id=6031075938")
 
+local fps = serv:Channel("Setting")
+
+local gui = Instance.new("ScreenGui")
+gui.Name = "DarkModeUI"
+gui.IgnoreGuiInset = true
+gui.DisplayOrder = 999
+gui.Parent = plr:WaitForChild("PlayerGui")
+
+local blackScreen = Instance.new("Frame")
+blackScreen.Size = UDim2.new(1, 0, 1, 0)
+blackScreen.Position = UDim2.new(0, 0, 0, 0)
+blackScreen.BackgroundColor3 = Color3.fromRGB(36, 36, 36)
+blackScreen.BackgroundTransparency = 0
+blackScreen.Visible = false
+blackScreen.Parent = gui
+
+local textLabel = Instance.new("TextLabel")
+textLabel.Size = UDim2.new(0, 300, 0, 100)
+textLabel.Position = UDim2.new(0.5, -150, 0.5, -50)
+textLabel.BackgroundTransparency = 1
+textLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+textLabel.TextScaled = false
+textLabel.TextSize = 32
+textLabel.RichText = true
+textLabel.Parent = blackScreen
+
+local gameMode = game:GetService("ReplicatedStorage"):WaitForChild("Values"):WaitForChild("Game"):WaitForChild("Gamemode")
+local gameSpeed = game:GetService("ReplicatedStorage"):WaitForChild("Values"):WaitForChild("Game"):WaitForChild("GameSpeed")
+local Difficulty = game:GetService("ReplicatedStorage"):WaitForChild("Values"):WaitForChild("Game"):WaitForChild("Difficulty")
+local level = game:GetService("ReplicatedStorage"):WaitForChild("Values"):WaitForChild("Game"):WaitForChild("Level")
+local autoPlay = game:GetService("ReplicatedStorage"):WaitForChild("Player_Data"):WaitForChild(plr.Name):WaitForChild("Data"):WaitForChild("AutoPlay")
+
+local function updateText()
+local autoPlayText = autoPlay.Value and "<font color='#00FF00'><b>Enable</b></font>" or "<font color='#FF0000'><b>Disable</b></font>"
+
+local diff
+    if Difficulty.Value == "Normal" then
+        diff = "#00FF00"
+    elseif Difficulty.Value == "Hard" then
+        diff = "#FF0000"
+    elseif Difficulty.Value == "Nightmare" then
+        diff = "#B700FF"
+    else
+        diff = "#FFFFFF"
+    end
+local diffText = "<font color='" .. diff .."'><b>" .. tostring(Difficulty.Value) .. "</b></font>"
+
+local speedColor
+    if gameSpeed.Value == 1 then
+        speedColor = "#00FF00"
+    elseif gameSpeed.Value == 2 then
+        speedColor = "#FFFF00"
+    elseif gameSpeed.Value == 3 then
+        speedColor = "#FF0000"
+    else
+        speedColor = "#FFFFFF"
+    end
+local speedText = "<font color='" .. speedColor .."'><b>" .. tostring(gameSpeed.Value) .. "x</b></font>"
+
+textLabel.Text = "<font color='#00FF00'><b>" .. plr.Name .. "</b></font>" ..
+                "\nGame Mode <font color='#FFFF00'><b>" .. gameMode.Value .. "</b></font>" ..
+                "\nStage <font color='#00EEFF'><b>" .. tostring(level.Value) .. "</b></font> | " .. diffText ..
+                "\nGame Speed " .. speedText ..
+                "\nAuto Play " .. autoPlayText
+end
+
+updateText()
+
+gameMode.Changed:Connect(updateText)
+gameSpeed.Changed:Connect(updateText)
+autoPlay.Changed:Connect(updateText)
+level.Changed:Connect(updateText)
+
+fps:Toggle("Dark Mode", false, function(b)
+    blackScreen.Visible = b
+end)
